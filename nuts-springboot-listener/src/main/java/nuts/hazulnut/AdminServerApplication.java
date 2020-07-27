87d02ab215b26c57470677bf7675b3c09634f564
Sleeber.Ann <anch8apparition@163.com>
2020-07-27 23:19:43 +0800

    Spring Boot Admin

---
+++ nuts-springboot-listener/src/main/java/nuts/hazulnut/AdminServerApplication.java
@@ -0,0 +1,24 @@
+package nuts.hazulnut;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Admin启动类.
+ *
+ * <p>Admin Server启动引导类
+ *
+ * @author <a href="https://blog.csdn.net/yidichaxiang" target="_blank">Sleeber</a>
+ * @since 1.3.0
+ *
+ * <p><b>Note:</b>
+ * <p>2020/7/27 21:48: created.<br>
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
